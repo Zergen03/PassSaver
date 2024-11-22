a3443ef8ce5f6787ff00c1a9c5dcc005312e1cc5
Alejandro Izquierdo Laguna <alexizquierdo2003@gmail.com>
2024-11-22 18:19:24 +0100

    first commit

---
+++ www/scr/main.js
@@ -0,0 +1,18 @@
+fetch("http://localhost:3000/categories")
+.then(res => res.json())
+.then(data => drawData(data))
+
+let drawData = (data) => {
+    console.log(data);
+    
+    data.forEach(category => {
+      let parent = document.getElementsByTagName('table')[0]
+      let child = document.createElement('tr')
+      child.innerText = category.name
+      child.addEventListener('click', () => {
+        alert(`has hecho click en ${category.name}`)
+      })
+      parent.appendChild(child)
+    })
+  }
+
