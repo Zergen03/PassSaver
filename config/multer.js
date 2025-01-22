f23de747657bcbb794e01f16d59175c34425274b
Alejandro Izquierdo Laguna <alexizquierdo2003@gmail.com>
2025-01-22 13:31:05 +0100

    back to commit before images

---
+++ config/multer.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const path = require('path');
+
+// Configuración de Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../www/uploads/'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + file.originalname;
+    cb(null, uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
